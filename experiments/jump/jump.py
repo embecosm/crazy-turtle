d19aca86c7bd9ce8d5661baa408de76bb9b1705f
Jeremy Bennett <jeremy.bennett@embecosm.com>
2015-03-08 15:35:01 +0000

    Working jump program.
    	* experiments/jump/jump.py: Better commenting.
    	(MotorJumpExample.__init__): Private instance variables for class
    	wide constants, timestamp, log variables and log variable
    	averages.
    	(MotorJumpExample._connected): Single log for all variables.
    	(MotorJumpExample._log_data): Keep all data in instance variables.
    	(MotorJumpExample._iir_filter): Created.
    	(MotorJumpExample._jump_motors): Simplified logic.

--- experiments/jump/jump.py
+++ experiments/jump/jump.py
@@ -25,7 +25,13 @@
 
 
 """
-Simple example that connects to the first Crazyflie found, powers up the motors for 500ms and disconnects.
+Module to make the Crazyflie hop
+
+Simple example that connects to the first Crazyflie found, powers up the
+motors for 500ms and disconnects.
+
+Classes exported:
+MotorJumpExample -- Instantiate to make the crazyflie hop
 """
 
 import os
@@ -40,7 +46,7 @@ try:
     sys.path.append(os.environ['CFLIB'])
 except:
     # Just catch and ignore
-    print "CFLIB not found"
+    print "CFLIB not specified"
 
 import cflib
 from cfclient.utils.logconfigreader import LogConfig
@@ -71,6 +77,23 @@ class MotorJumpExample:
         self._cf.connection_failed.add_callback(self._connection_failed)
         self._cf.connection_lost.add_callback(self._connection_lost)
 
+        # Log instantaneous data
+        self._timestamp = 0
+        self._asl       = 0.0
+        self._roll      = 0.0
+        self._pitch     = 0.0
+        self._yaw       = 0.0
+        self._thrust    = 0
+
+        # Log rolling averages
+        self._asl_av   = 0.0
+        self._roll_av  = 0.0
+        self._pitch_av = 0.0
+        self._yaw_av   = 0.0
+
+        # Rolling average is done as an infinite impulse response filter
+        self._iir_coeff = 0.8
+
         # Opening the connection will cause the behavior to happen
         self._cf.open_link(link_uri)
 
@@ -90,41 +113,41 @@ class MotorJumpExample:
         link_uri -- the URI for the Crazyflie (no default)
         """
 
-        # Initially we go up and have done nothing with the barometer log
-        self._thrust_mult = 1
-        self._baro_count = 0;
-        self._initial_baro_average = 0;
-        self._baro_average = 0;
-
-        # Start the barometer logging.
+        # Start the logging.
 
         # The definition of the logconfig could have been made before
         # connecting, but we do it here, since the rest of the intialization
         # can't be done until we are connected.
-        self._lg_baro = LogConfig(name = "Baro", period_in_ms = 100);
-        self._lg_baro.add_variable("baro.aslLong", "float");
+        self._lg = LogConfig(name = "Log", period_in_ms = 20);
+        self._lg.add_variable("baro.aslLong", "float");
+        self._lg.add_variable("stabilizer.roll", "float")
+        self._lg.add_variable("stabilizer.pitch", "float")
+        self._lg.add_variable("stabilizer.yaw", "float")
+        self._lg.add_variable("stabilizer.thrust", "uint16_t")
+
+        self._first_datapoint = True;
 
         # Adding the configuration cannot be done until a Crazyflie is
         # connected, since we need to check that the variables we
         # would like to log are in the TOC.
-        self._cf.log.add_config(self._lg_baro)
-        if self._lg_baro.valid:
+        self._cf.log.add_config(self._lg)
+        if self._lg.valid:
             # This callback will receive the data
-            self._lg_baro.data_received_cb.add_callback(self._baro_log_data)
+            self._lg.data_received_cb.add_callback(self._log_data)
             # This callback will be called on errors
-            self._lg_baro.error_cb.add_callback(self._baro_log_error)
+            self._lg.error_cb.add_callback(self._log_error)
             # Start the logging
-            self._lg_baro.start()
+            self._lg.start()
         else:
-            print("Could not add baro logconfig")
+            print "Could not add logconfig"
 
         # Start a separate thread to do the motor test.
         # Do not hijack the calling thread!
         Thread(target=self._jump_motors).start()
 
 
-    def _baro_log_error(self, logconf, msg):
-        """Callback from the baro log API when an error occurs
+    def _log_error(self, logconf, msg):
+        """Callback from the log API when an error occurs
 
         We just note that an error has occurred.
 
@@ -133,22 +156,61 @@ class MotorJumpExample:
         msg     -- message to print out.
         """
 
-        print "Error when logging baro %s: %s" % (logconf.name, msg)
+        print "Error when logging %s: %s" % (logconf.name, msg)
 
 
-    def _baro_log_data(self, timestamp, data, logconf):
-        """Callback from the baro log API when data arrives
+    def _log_data(self, timestamp, data, logconf):
+        """Callback from the log API when data arrives
 
-        We print out the data from the barameter.
+        We print out the data from the log.
 
         Keyword arguments:
         timestamp -- when the log data occurred
         data      -- the actual data
         logconf   -- configuration data for this log
-
-
         """
-        print "[%d][%s]: %f" % (timestamp, logconf.name, data['baro.aslLong'])
+
+        #  Log data. Remember pitch is negative!
+        self._asl = data['baro.aslLong']
+        self._roll    = data['stabilizer.roll']
+        self._pitch   = - data['stabilizer.pitch']
+        self._yaw     = - data['stabilizer.yaw']
+        self._thrust  = data['stabilizer.thrust']
+
+        # Rolling averages. Start off with the value we have.
+        self._timestamp   = timestamp
+        if self._first_datapoint:
+            self._asl_av  = self._asl
+            self._roll_av     = self._roll
+            self._pitch_av    = self._pitch
+            self._yaw_av      = self._yaw
+
+            self._first_datapoint = False;
+        else:
+            self._asl_av   = self._iir_filter(self._asl_av,   self._asl)
+            self._roll_av  = self._iir_filter(self._roll_av,  self._roll)
+            self._pitch_av = self._iir_filter(self._pitch_av, self._pitch)
+            self._yaw_av   = self._iir_filter(self._yaw_av,   self._yaw)
+
+        print "[%d]: %f (%f %f %f)"  % (timestamp,
+                                        self._asl_av,
+                                        self._roll_av,
+                                        self._pitch_av,
+                                        self._yaw_av)
+
+    def _iir_filter(self, point, prev_res):
+        """Apply the a simple infinite impulse response filter
+
+          X[n] = (1-a).x[n] + a.X[n-1e]
+
+        Keyword arguments:
+        point    - the data point to add
+        prev_res - the result prior to this point.
+
+        Returns the updated IIR filter result
+        """
+
+        return (1.0 - self._iir_coeff) * point + prev_res * self._iir_coeff
 
 
     def _connection_failed(self, link_uri, msg):
@@ -161,6 +223,7 @@ class MotorJumpExample:
         msg      -- message to print out
         """
         print "Connection to %s failed: %s" % (link_uri, msg)
+
 
     def _connection_lost(self, link_uri, msg):
         """Callback when disconnected after a connection has been made
@@ -192,31 +255,78 @@ class MotorJumpExample:
         Just a burst of the motors.
         """
 
+        # Kill parameters
+        kill = False
+        cutoff = 20.0
+        cutoff_en = True
+
         # Parameters
-        thrust_step = 500
+        # Roll - negative makes it go left
+        # Pitch - positive makes it go forward
+        thrust_step  =   500
         thrust_start = 20000
-        thrust_stop  = 40000
-        pitch = 15
-        roll = 0
-        yawrate = 0
+        thrust_stop  = 37000
+        thrust_limit = 50000
+
+        # Target as a fraction of the take off ASL
+        asl_frac = 0.995
+
+        roll  = -2
+        pitch = 5
+        yaw   = 0
+
+        step_period = 0.05
+        jumptime = 3
+        jumpsteps = int(float(jumptime) / step_period)
 
         # Unlock startup thrust protection
         self._cf.commander.send_setpoint(0, 0, 0, 0)
 
         # Rapidly wind up
         for t in range(thrust_start, thrust_stop, thrust_step):
-            self._cf.commander.send_setpoint(roll, pitch, yawrate, t)
+            self._cf.commander.send_setpoint(roll, pitch, yaw, t)
             time.sleep(0.01)
 
-        # Wait a little bit
-        time.sleep(1.0)
+        self._aslTarget = self._asl * asl_frac
+        print "Target ASL %f" % (self._aslTarget)
+
+        thrust = thrust_stop
+        dircount = 0
+        prev_asl = self._asl_av
+
+        # Then new figures every second
+        for i in range (1, jumpsteps, 1):
+            if (cutoff_en and (abs(self._roll_av)  > cutoff or
+                               abs(self._pitch_av) > cutoff)):
+                print "Cutoff (%f %f)" % (self._roll_av, self._pitch_av)
+                kill = True
+
+            if kill:
+                # Die politely
+                print "Kill switch"
+                self._cf.commander.send_setpoint(0, 0, 0, 0)
+                time.sleep(1.0)
+                self._cf.close_link()
+            else:
+                print "Correcting: (%f %f %f) %d"  % (roll  - self._roll_av,
+                                                      pitch - self._pitch_av,
+                                                      yaw   - self._yaw_av,
+                                                      thrust)
+
+                self._cf.commander.send_setpoint(roll  - self._roll_av,
+                                                 pitch - self._pitch_av,
+                                                 yaw   - self._yaw_av,
+                                                 thrust)
+
+            # Wait a little bit
+            time.sleep(step_period)
 
         # Turn the motors off
         self._cf.commander.send_setpoint(0, 0, 0, 0)
 
         # Make sure that the last packet leaves before the link is closed
         # since the message queue is not flushed before closing
-        time.sleep(0.1)
+        time.sleep(1.0)
         self._cf.close_link()
 
 
